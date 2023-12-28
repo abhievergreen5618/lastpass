b52725de97167334ea07c1c03c4a0e7b9c9e2d49
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-12-28 15:35:17 +0530

    update

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\FolderSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +24,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             FolderSeeder::class,
+            UserSeeder::class,
             // Add more seeders if needed
         ]);
     }
