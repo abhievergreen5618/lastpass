280d2e268afaeb7768cdaf63218d3c8a32cb6e3f
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-01-10 17:46:12 +0530

    Update Kernel.php

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-    $schedule->command('truncate:recent')->everyMinute();
+        $schedule->command('truncate:recent')->hourly();
     }
 
     /*
