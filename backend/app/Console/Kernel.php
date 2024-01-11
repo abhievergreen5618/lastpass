cae67d9faf164fd870c4c3289789846c6206bd80
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-01-11 10:00:54 +0530

    truncaterecent

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('truncate:recent')->hourly();
+        $schedule->command('app:truncate-recent-data')->hourly();
     }
 
     /*
