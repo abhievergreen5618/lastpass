f927c5206423b545558a1c487b926f0a47cc77f5
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-01-12 17:18:23 +0530

    phonecodelist

--- backend/app/Http/Controllers/Api/PhoneCodeController.php
+++ backend/app/Http/Controllers/Api/PhoneCodeController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
+use App\Models\PhoneCode;
 
 class PhoneCodeController extends Controller
 {
     //
+    public function index() {
+        $phonecode = PhoneCode::get();
+    
+        if (!empty($phonecode)) {
+            return response()->json(['phonecode' => $phonecode]);
+        } else {
+            // If no folders are found, return a message
+            return response()->json(['phonecode' => ['No phonecode founded']]);
+        }
+    }
 }
