0702c205f4e4a9025dbc986e2f9fe5f2c0dda988
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-01-12 16:05:21 +0530

    migratetable

--- backend/app/Models/Country.php
+++ backend/app/Models/Country.php
@@ -10,7 +10,7 @@ class Country extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id',
+        
         'name',
     ];
 }
--- backend/app/Models/PhoneCode.php
+++ backend/app/Models/PhoneCode.php
@@ -10,7 +10,7 @@ class PhoneCode extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id',
+        
         'countrycode',
         'name',
         'phonecode',
