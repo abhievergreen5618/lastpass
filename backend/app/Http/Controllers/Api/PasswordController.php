a97b5c381fc599a3de46f6fa3635bd1eed5bfe01
shivangi980 <113875231+shivangi980@users.noreply.github.com>
2024-01-24 19:12:15 +0530

    Update PasswordController.php

--- backend/app/Http/Controllers/Api/PasswordController.php
+++ backend/app/Http/Controllers/Api/PasswordController.php
@@ -155,7 +155,7 @@ class PasswordController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Request $request, string $id)
     {
         try 
         {
